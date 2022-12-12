93f4ed7be71f540d20a8f27ab24aff0ec2df59c8
walksanatora <walkerffo22@gmail.com>
2022-12-12 23:15:39 +0000

    can i finally commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![feature(decl_macro)]
+#[macro_use] extern crate rocket;
+
+use rocket::fs::NamedFile;
+use rocket::http::ContentType;
+use rocket::response::status::NotFound;
+use rocket::serde::json::Json;
+use std::fs::read_dir;
+
+
+#[get("/download/<t>")]
+async fn branches(t: String) -> Result<Json<Vec<String>>,NotFound<String>>  {
+    let branches = read_dir(format!("files/{}",t));
+    if let Ok(files) = branches {
+        let output = files.map(|f|{f.unwrap().file_name().into_string().unwrap()}).collect();
+        Ok(Json(output))
+    } else {
+        Err(NotFound("Invalid type".into()))
+    }
+    
+}
+
+#[get("/download/<t>/<branch>")]
+async fn download(t: String,branch: String) -> Result<(ContentType,NamedFile), NotFound<String>> {
+    let ext = match t.as_str() {
+        "installer" => "lua",
+        "orangebox" => "vgz",
+        "yellowbox" => "vfs",
+        _ => ""
+    };
+    let ct = match t.as_str() {
+        "installer" => ContentType::parse_flexible("application/x-lua").unwrap(),
+        "orangebox" => ContentType::parse_flexible("application/gzip+txt").unwrap(),
+        "yellowbox" => ContentType::parse_flexible("application/octet-stream").unwrap(),
+        _ => ContentType::default()
+    };
+    let file = format!("files/{}/{}.{}",t,branch,ext);
+    let nf= NamedFile::open(file).await.map_err(|e| NotFound(e.to_string()));
+    match nf {
+        Ok(named) => {Ok((ct,named))},
+        Err(e) => {Err(e)},
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![branches,download])
+}
