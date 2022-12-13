caba043d306c7dbf115152e5fb20b88f641383c4
walksanatora <walkerffo22@gmail.com>
2022-12-13 06:58:42 +0000

    ignore the tem files. add listing of targets

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(decl_macro)]
 #[macro_use] extern crate rocket;
 
-use chrono::prelude::*;
+use chrono::{prelude::*, Duration};
 use once_cell::sync::OnceCell;
 use rocket::fs::NamedFile;
 use rocket::http::{ContentType, Status};
@@ -14,7 +14,9 @@ use std::sync::Mutex;
 
 struct State {
     last_fetch: NaiveTime,
-    last_build: NaiveTime
+    fetch_delay: Duration,
+    last_build: NaiveTime,
+    build_delay: Duration
 }
 
 static STATE: OnceCell<Mutex<State>> = OnceCell::new();
@@ -29,10 +31,29 @@ struct Message{
 async fn trigger(trig: String) -> (Status,Json<Message>) {
     match trig.as_str() {
         "rebuild" => {
-            (Status::NotImplemented,Json(Message { message: "Not implemented yet".into(), code: 501 }))
+            let state = STATE.get().unwrap().lock().unwrap();
+            let rtime = Local::now().time() - state.last_build;
+            if rtime > state.build_delay {
+                (Status::NotImplemented,Json(Message { message: "Not implemented yet".into(), code: 501 }))
+            } else {
+                (Status::TooManyRequests,Json(Message {message: format!("CI only alloys rebuilds every {},{} remaining",rtime,rtime-state.build_delay), code: 429}))
+            }
         }
         _ => {(Status::BadRequest,Json(Message { message: "Invalid Trigger".into(), code: 400 }))}
     }
+}
+
+
+#[get("/download")]
+async fn targets() -> Json<Vec<String>>  {
+    let branches = read_dir("files/").unwrap();
+    let output = branches.map(|f|{
+        f.unwrap()
+        .file_name()
+        .into_string()
+        .unwrap()
+    }).collect();
+    Json(output) 
 }
 
 #[get("/download/<target>")]
@@ -53,8 +74,7 @@ async fn branches(target: String) -> Result<Json<Vec<String>>,NotFound<String>> 
         Ok(Json(output))
     } else {
         Err(NotFound("Invalid target".into()))
-    }
-    
+    } 
 }
 
 #[get("/download/<target>/<branch>")]
@@ -81,6 +101,11 @@ async fn download(target: String,branch: String) -> Result<(ContentType,NamedFil
 
 #[launch]
 fn rocket() -> _ {
-    let _ = STATE.set(Mutex::new(State { last_fetch: NaiveTime::default(), last_build: NaiveTime::default() }));
-    rocket::build().mount("/", routes![branches,download,trigger])
+    let _ = STATE.set(Mutex::new(State { 
+        last_fetch: NaiveTime::default(), 
+        fetch_delay: Duration::minutes(30), 
+        last_build: NaiveTime::default(), 
+        build_delay: Duration::minutes(30) 
+    }));
+    rocket::build().mount("/", routes![targets,branches,download,trigger])
 }
